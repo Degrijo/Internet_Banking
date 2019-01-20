8c46a3a0a8b956c29058a2d7472c6e166f9039bf
Yatsenko Yaroslav <yarikyatsenko@mail.ru>
2019-01-20 14:13:31 +0300

    working news

--- news/models.py
+++ news/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
-    post = models.TextField()
+    text = models.TextField()
+    author = models.CharField(max_length=100, default="anonymous")
 
     def __str__(self):
         return self.title
